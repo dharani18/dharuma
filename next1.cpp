33a81accd0e5d95621653743d7ca17ec12f58ab7
dharani18 <36472689+dharani18@users.noreply.github.com>
2018-05-25 12:34:57 +0530

    Create next1.cpp

---
+++ next1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int n,a,b;
+   cin>>n;
+   if(n>0)
+       {
+       a=n+1;
+       cout<<a;
+       }
+   else if(n<0)
+   {
+      b=n-1;
+      cout<<b;
+   }
+   return 0;
+}
